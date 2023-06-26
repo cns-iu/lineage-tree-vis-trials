5415954d51e95dcbe106c9a565be25639b43c2b4
Bruce W. Herr II <bherr@indiana.edu>
2023-06-26 19:52:52 -0400

    Updated layout, pipeline, and rearranged

--- src/newick2dot.py
+++ src/newick2dot.py
@@ -1,3 +1,5 @@
+import argparse
+from csv import DictReader
 from sys import argv
 
 import networkx as nx
@@ -5,32 +7,58 @@ from Bio import Phylo
 from networkx.drawing.nx_agraph import write_dot
 from networkx.relabel import convert_node_labels_to_integers
 
-INPUT=argv[1]
-OUTPUT=argv[2]
 
-phylo_tree = Phylo.read(INPUT, "newick")
-tree = Phylo.to_networkx(phylo_tree)
+def _get_arg_parser():
+    parser = argparse.ArgumentParser(
+        description='Convert newick to map4sci dot format')
+    parser.add_argument(
+        '--mapping', help='cell => type label mappings in csv format')
+    parser.add_argument('--node-weights', help='How to weight nodes',
+                        choices=['pagerank', 'betweenness'], required=True)
+    parser.add_argument('input', help='Input newick file.')
+    parser.add_argument('output', help='Output dot file')
+    return parser
 
-G = nx.Graph()
 
-for (n, data) in tree.nodes.data():
-  G.add_node(n.name, label=n.name, weight=0)
+def main(args):
+    INPUT = args.input
+    OUTPUT = args.output
+    MAPPING = args.mapping
+    WEIGHTS = args.node_weights
 
-max_weight = max( weight for (_, _, weight) in tree.edges.data('weight') )
-for (source, target, weight) in tree.edges.data('weight'):
-  G.add_edge(source.name, target.name, weight=weight / max_weight * 1000)
+    labels = {}
+    if MAPPING:
+        for row in DictReader(open(MAPPING)):
+            labels[row['cell']] = row['type']
 
-print(G.number_of_nodes(), G.number_of_edges())
+    phylo_tree = Phylo.read(INPUT, "newick")
+    tree = Phylo.to_networkx(phylo_tree)
 
-pagerank_items = nx.pagerank(G, weight='weight').items();
-max_rank = max( rank for (_n, rank) in pagerank_items)
-for (n, rank) in pagerank_items:
-  G.nodes[n]['weight'] = rank / max_rank * 1000
+    G = nx.Graph()
 
-# for (n, centrality) in nx.betweenness_centrality(G, weight='weight', normalized=False).items():
-#   G.nodes[n]['weight'] = centrality
+    for n in tree.nodes():
+        G.add_node(n.name, label=labels.get(str(n.name), n.name), weight=0)
 
-G = G.subgraph(max(nx.connected_components(G), key=len)).copy()
-H = convert_node_labels_to_integers(G, 1, 'decreasing degree', 'label')
+    max_weight = max(weight for (_, _, weight) in tree.edges.data('weight'))
+    for (source, target, weight) in tree.edges.data('weight'):
+        G.add_edge(source.name, target.name, weight=weight / max_weight * 1000)
 
-write_dot(H, OUTPUT)
+    if WEIGHTS == 'pagerank':
+        pagerank_items = nx.pagerank(G, weight='weight').items()
+        max_rank = max(rank for (_n, rank) in pagerank_items)
+        for (n, rank) in pagerank_items:
+            G.nodes[n]['weight'] = rank / max_rank * 1000
+    elif WEIGHTS == 'betweenness':
+        for (n, centrality) in nx.betweenness_centrality(G, weight='weight', normalized=False).items():
+            G.nodes[n]['weight'] = centrality
+
+    G = G.subgraph(max(nx.connected_components(G), key=len)).copy()
+    H = convert_node_labels_to_integers(G, 1, 'decreasing degree', 'cell')
+
+    write_dot(H, OUTPUT)
+
+
+if __name__ == "__main__":
+    parser = _get_arg_parser()
+    args = parser.parse_args()
+    main(args)
